606d98fbf66836b77a0ac02a305b279f1c3c3a08
jsakshatha <jsakshatha.shettigar@gmail.com>
2026-02-26 22:22:39 +0530

    Update 24_feb_wde.sql

--- 24_feb_wde.sql
+++ 24_feb_wde.sql
@@ -177,5 +177,145 @@ alter table employee change emp_gender gender varchar(11);
 
 alter table employee rename to employee_info;
 
+
+
+-- 26 FEB 2026
+
+show databases;
+
+-- select the database
+use app_db;
+
+-- verify table name
+show tables;
+
+alter table employee_info rename to employee;
+
+select * from employee;
+
+-- 1.find the maximum salary
+select max(emp_salary)
+from employee;
+
+-- 2.find the minimum salary-min(field_name)
+select min(emp_salary)
+from employee;
+
+-- 3.find the average salary-avg(field_name)
+select avg(emp_salary)
+from employee;
+
+-- 4.find the total salary--sum(field_name)
+select sum(emp_salary)
+from employee;
+
+-- 5.find the number of employee- count(field_name)
+select count(emp_id)
+from employee;
+
+-- max,min,avg,sum,count--- aggregate functions
+
+-- 6.find the department names--unique
+
+select distinct emp_dept
+from employee;
+
+select * from employee;
+-- 7.find the number of employees in each dept
+
+select count(emp_id) as no_of_employees ,emp_dept
+from employee
+group by emp_dept;
+
+-- 8.find max salary based on dept wise
+SELECT max(emp_salary) AS Maximum_salary, emp_dept
+FROM   employee
+GROUP BY emp_dept;
+
+-- 9.find the total salary in each dept 
+SELECT SUM(emp_salary) AS total_salary, emp_dept
+FROM   employee
+GROUP BY emp_dept;
+
+
+select max(emp_salary),min(emp_salary),emp_dept
+from employee
+group by emp_dept;
+
+
+-- 10 sort the salary in ascending order
+select emp_salary
+from employee
+order by emp_salary asc;
+
+-- 11 sort the salary in descending order usind desc command
+select emp_salary
+from employee
+order by emp_salary desc;
+
+
+
+
+
+
+
+
+
+-- 12. fetch the top 3 datas(rows)
+select *
+from employee
+limit 3;
+
+-- 13. fetch last 3 datas(rows)
+select * from employee
+order by emp_id desc
+limit 3;
+
+-- 14.find the details of the employee who's getting highest paid salary
+
+select * 
+from employee 
+order by emp_salary desc 
+limit 1;
+
+
+-- 15.find the details of the employee who belongs to HR dept 
+select * 
+from employee
+where emp_dept="HR";
+
+-- 16.FIND THE DETAILS OF THE EMPLOYEE WHO'S SALARY IS GREATER THAN 50000
+select * 
+from employee
+where emp_salary >50000;
+
+-- 17.find the details of the employee who belongs to HR dept and salary is morethan 75000
+select * 
+from employee
+where emp_dept="HR" and emp_salary > 75000;
+
+-- 18. find the details of the employee who belongs to HR,SALES
+-- solution 1
+select *
+from employee
+where emp_dept="HR" or emp_dept="sales";
+
+-- solution 2
+select *
+from employee
+where emp_dept in ("hr","it");
+
+-- 19.find the details of the employee earning between 20000 to 50000
+select * 
+from employee
+where emp_salary between 20000 and 50000;
+
+-- 20.find the details of the employee who's age is null
+select *
+from employee
+where emp_age is null;
+
+
+
 select * from employee_info;
 
